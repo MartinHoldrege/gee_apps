1ce56546022f796db3322f1eac37e6180e1ee6f7
martinholdrege <martinholdrege@gmail.com>
2024-10-15 02:13:16 +0000

    Updated src/fig_functions.js (added 37 lines, deleted 1 line).

---
+++ src/fig_functions.js
@@ -0,0 +1,37 @@
+/*
+Description: functions for visualizations
+
+
+*/
+
+// creating legends -------------------------------------------------------------
+
+
+// Creates and styles 1 row of the legend.
+var makeRow = function(color, name) {
+ 
+      // Create the label that is actually the colored box.
+      var colorBox = ui.Label({
+        style: {
+          backgroundColor: color,
+          // Use padding to give the box height and width.
+          padding: '4px',
+          margin: '0 0 1px 0'
+        }
+      });
+ 
+      // Create the label filled with the description text.
+      var description = ui.Label({
+        value: name,
+        style: {
+          margin: '0 0 1px 1px',
+          fontSize: '11px'
+        }
+      });
+ 
+      // return the panel
+      return ui.Panel({
+        widgets: [colorBox, description],
+        layout: ui.Panel.Layout.Flow('horizontal')
+      });
+};
