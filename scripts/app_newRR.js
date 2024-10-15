8c844e3e16c0f3625bfe83ee0ff7af6dfdcc3ee1
martinholdrege <martinholdrege@gmail.com>
2024-10-15 01:59:42 +0000

    Updated scripts/app_newRR.js (added 24 lines, deleted 3 lines).

--- scripts/app_newRR.js
+++ scripts/app_newRR.js
@@ -9,8 +9,29 @@ Started: October 14, 2024
 // dependencies -----------------------------------------------------------------------------
 
 
+// params ---------------------------------------------------------------------------------
 
+var path = 'projects/ee-martinholdrege/assets/misc/newRR3/'
 
 // read in layers ---------------------------------------------------------------------------
+
+// for testing
+var imagetype1 = ee.Image(path + 'Resil-cats_2029-2064-RCP45');
+var imagetype2 = ee.Image(path + 'Resil-cont_2029-2064-RCP45');
+var imagetype3 = ee.Image(path + 'Resil-cont_2029-2064-RCP45-delta');
+
+// fig params --------------------------------------------------------------
+
+ui.root.clear();
+//var panel = ui.Panel({style: {width: '250px'}});
+var map = ui.Map();
+//ui.root.add(panel).add(map); // order that you add panl vs map affects if panel is right or left
+ui.root.add(map); 
+
+map.style().set('cursor', 'crosshair');
+
+// legends -----------------------------------------------------------------------------
+
+
 
 
