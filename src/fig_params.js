6694da70e97eb9d9bdf7468a3a14e7ba97ef692c
martinholdrege <martinholdrege@gmail.com>
2024-10-15 02:35:32 +0000

    Updated src/fig_params.js (added 40 lines, deleted 32 lines).

--- src/fig_params.js
+++ src/fig_params.js
@@ -6,39 +6,47 @@
   Started: Oct 14, 2024
 */
 
-// Creates and styles 1 row of the legend.
-var makeRow = function(color, name) {
- 
-      // Create the label that is actually the colored box.
-      var colorBox = ui.Label({
-        style: {
-          backgroundColor: color,
-          // Use padding to give the box height and width.
-          padding: '6px',
-          margin: '0 0 1px 0'
-        }
-      });
- 
-      // Create the label filled with the description text.
-      var description = ui.Label({
-        value: name,
-        style: {
-          margin: '0 0 1px 1px',
-          fontSize: '12px'
-        }
-      });
- 
-      // return the panel
-      return ui.Panel({
-        widgets: [colorBox, description],
-        layout: ui.Panel.Layout.Flow('horizontal')
-      });
-};
- 
+// colors ---------------------------------------------------------------------------------
 
+// type 2 RR colors (i.e categorical RR)
+var rrT2Palette = ['#D7191C', '#FDAE61', '#ABD9E9', '#2C7BB6']; 
+var rrT2Names = ['L', 'ML', 'M', 'H+MH'];
+
+// mapping visualizing elements -----------------------------------------------------------
+
+exports.visT2 = {"opacity":1,"min":1,"max":4, "palette":rrT2Palette};
+
+// building legends ------------------------------------------------------------------------
+
+
+ 
+ 
+// categorical RR legend (i.e., type 2 colors)
+
+// set position of panel
+var legendT2 = ui.Panel({
+  style: {
+    position: 'bottom-left',
+    padding: '8px 12px'
+  }
+});
+ 
+// Create legend title
+var legendTitleT2 = ui.Label({
+  value: 'Categorical',
+  style: {
+    fontWeight: 'bold',
+    fontSize: '11px',
+    margin: '0 0 4px 0',
+    padding: '0'
+    }
+});
+ 
+// Add the title to the panel
+legendT2.add(legendTitleT2);
 // Add color and and names
 for (var i = 0; i < c9Palette.length; i++) {
-  legend.add(makeRow(c9Palette[i], c9Names[i]));
+  legendT2.add(makeRow(rrT2Palette[i], rrT2Names[i]));
   }  
  
-exports.legendc9 = legend;
+exports.legendT2 = legendT2;
