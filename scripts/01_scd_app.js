a8f26fd83fd1a2da5892cbc86def42d35066b81e
martinholdrege <martinholdrege@gmail.com>
2024-10-22 23:21:36 +0000

    WIP

--- scripts/01_scd_app.js
+++ scripts/01_scd_app.js
@@ -1,9 +1,9 @@
 /********************************************************
  * Purpose:
- * simple EE app to be used by for the the sagebrush climate training, to show 
- * some of the main layers presented in the REM manuscript
+ * Simple app to show basic layers shown in Holdrege et al. 2024 (rangeland ecology and management)
+ * Based on the SEI/scripts/06_app_sage-climate-training.js app.
  * 
- * Script Started: May 17, 2023
+ * Script Started: October 22, 2024
  * 
  * Author: Martin Holdrege
  * 
@@ -22,6 +22,7 @@ var root = 'fire1_eind1_c4grass1_co20_2311_';
 // The functions, lists, etc are used by calling SEI.nameOfObjectOrFunction
 var SEI = require("users/MartinHoldrege/SEI:src/SEIModule.js");
 var fig = require("users/MartinHoldrege/SEI:src/fig_params.js");
+var f = require("users/MartinHoldrege/gee_apps:src/general_functions.js");
 
 // this is where the data wrangling occurs
 // contains one main function
@@ -110,48 +111,177 @@ map.addLayer(ee.Image(d.get('p')).select('p6_c9Med'), fig.visc9, '9 class transi
 // 'backgroud' layers ---------------------------------------------------------------------------
 map.addLayer(fig.statesOutline, {}, 'state outlines', false); // outline of states (white background)
 
-// misc labels -------------------------------------------------------------------------
+// description panel -------------------------------------------------------------------------------------
 
-// label providing simulations settings
+// style elements for text
+var mt = '10px'; var mr = ' 10px'; var mb = ' 10px'; var ml = ' 10px'; // top, right, bottom and left margins
+var margin = mt + mr + mb + ml;
+var marginNoTB = '0px' + mr + ' 0px' + ml; // no top and bottom margines
+var marginNoT = '0px' + mr + mb + ml; // no top  margin's
+var marginNoB = mt + mr + ' 0px' + ml; // no bottom  margin's
+var fontSizeText = '11px';
+var styleText = {fontSize: fontSizeText, margin: mt + mr + mb + ml};
+var styleTextNoTB =  f.updateDict(styleText, 'margin', marginNoTB);
+var styleHeader = {fontSize: '15px', fontWeight: 'bold'};
+
+var styleUrl = {
+  fontSize: fontSizeText, 
+  color: 'blue', 
+  textDecoration: 'underline',
+  margin: marginNoTB
+};
+
+// Set up title and summary widgets
+
+//App title
+var title = ui.Label('Sagebrush Ecological Integrity Projections', 
+  {fontSize: '18px', fontWeight: 'bold', color: '4A997E'});
+
+// Create a panel to hold text
 var panel = ui.Panel({
-  style: {
-    position: 'bottom-right',
-    padding: '8px 15px'
-  }
-});
- 
-// Create legend title
-var firstLine = ui.Label({
-  value: 'Simulations done for ' + d.get('RCP').getInfo() 
-    + ', ' + d.get('epoch').getInfo() + ' (default STEPWAT2 assumptions used)',
-  style: {
-    fontSize: '12px',
-    margin: '0 0 0 0', // Adjust margin as needed
-    padding: '0'
-  }
+  widgets:[title],//Adds header and text
+  style:{width: '300px',position:'middle-left'}});
+
+
+// text for the main panel
+
+// first paragraph
+var par1 = 'This app visualizes potential future changes in sagebrush ecological' + 
+' integrity (SEI) under a range of scenarios using an individual plant-based' + 
+' simulation model, integrated with remotely sensed estimates of current SEI.' + 
+' The simulation model (STEPWAT2) allowed us to estimate how climate change,' + 
+' wildfire, and invasive annuals interact to alter the potential abundance of key' + 
+' plant functional types that influence sagebrush ecological integrity: sagebrush,' + 
+' perennial forbs and grasses, and annual forbs grasses. Here we provide' + 
+' visualizations of projected changes in SEI and SEI class (Core Sagebrush Areas,' + 
+' Growth Opportunity Areas, and Other Rangeland Areas). Additionally, we show' + 
+' projections of changes of three of the components of sagebrush ecological' + 
+' integrity, which are the Q (‘quality’) scores for sagebrush, perennial forbs' + 
+' and grasses, and annual forbs and grasses. ';
+
+// 2nd paragraph
+var par2a =    ui.Label({
+    value: 'Data shown are available from ScienceBase',
+    style: styleTextNoTB, // no bottom margin
+  });
+
+var par2b =    ui.Label({
+    value: '(https://doi.org/10.5066/P13RXYZJ).',
+    targetUrl: 'https://doi.org/10.5066/P13RXYZJ',
+    style: styleUrl
+  });
+
+var par3a = ui.Label({
+    value: 'Further details about the research that developed these projections' + 
+    ' are available in Holdrege et al. (2024)',
+    style: f.updateDict(styleText, 'margin', marginNoB), // no bottom margin
+  });
+
+
+var remLink = ui.Label({
+    value: '(https://doi.org/10.1016/j.rama.2024.08.003).',
+    targetUrl: 'https://doi.org/10.1016/j.rama.2024.08.003',
+    style: f.updateDict(styleUrl, 'margin', marginNoT)
+  });
+
+// how to use
+var howTo = 'Select layer(s) to view from the dropdown "Layers" menu.';
+
+// abbrevations --------------------------
+var abbrevExplain = ui.Label({
+    value: 'Abbreviations:',
+    style: f.updateDict(styleText, 'margin', marginNoT), // 
+  });
+
+// bullets describing the abbreviation
+var bullets1Text = [
+  'SEI=Sagebrush Ecological Integrity',
+  'CSA=Core Sagebrush Area (an area with high SEI)',
+  'GOA=Growth Opportunity Area (an area with intermediate SEI)',
+  'ORA=Other Rangeland Area (an area with low SEI)'
+  ];
+  
+var bullets1Text = [
+  'SEI: Sagebrush Ecological Integrity',
+  'CSA: Core Sagebrush Area (an area with high SEI)',
+  'GOA: Growth Opportunity Area (an area with intermediate SEI)',
+  'ORA: Other Rangeland Area (an area with low SEI)',
+  'GCM: Global Climate Model',
+  'RCP: Representative Concentration Pathway'
+  ];
+  
+var layersExplain = ui.Label({
+    value: 'Description of layers:',
+    style: f.updateDict(styleText, 'margin', marginNoB) 
+  });
+  
+// continue here: 
+var bullets1Text = [
+  '9 class transition:  Median change in sagebrush ecological integrity (SEI)' + 
+  ' classification from current (2017-2021) to future (RCP 4.5, 2071-2100) climate conditions',
+  '3 Class SEI: Extent of CSA, GOAs and ORAs during 2017-2020 (layer from Doherty et al. 2022)'
+  ' Agreement among GCMs: Agreement among climate models for change in SEI classification (under RCP4.5, 2071-2100)' + 
+  ' for areas that are currently CSAs or GOAs.' + 
+  ' ‘Non-robust agreement’ indicates agreement among 7-11 models out of 13 (light colors,' + 
+  ' not a robust signal), and ‘robust agreement’ means agreement among 12-13 models' + 
+  ' (dark colors, a robust signal). Loss of CSA means future classification is GOA or ORA.' + 
+  ' Loss of GOA means future classification is ORA.,
+  'GOA: Growth Opportunity Area (an area with intermediate SEI)',
+  'ORA: Other Rangeland Area (an area with low SEI)'
+  ];
+  
+var bulletsLabel = bulletsText.map(function(x) {
+  return ui.Label({
+    value: '• ' + x,
+    style: {fontSize: fontSizeText, margin: '0px' + mr + ' 0px 20px'},
+  });
 });
 
-var secondLine = ui.Label({
-  value: '(unless otherwise noted, values are medians across 13 GCMs)',
-  style: {
-    fontSize: '12px',
-    margin: '0 0 4px 0', // Adjust margin as needed
-    padding: '0'
-  }
+var bulletsPanel = ui.Panel({
+  widgets: [abbrevExplain].concat(bulletsLabel).concat([abbrevExample]),
+  layout: ui.Panel.Layout.Flow('vertical')
 });
 
-// Create a panel to hold both labels and stack them vertically.
-var panelDescript = ui.Panel({
-  widgets: [firstLine, secondLine],
-  layout: ui.Panel.Layout.flow('vertical'),
-  style: {
-    padding: '0'
-  }
-});
- 
-// Add the title to the panel
-panel.add(panelDescript);
-map.add(panel);
+var description = ui.Panel([
+  ui.Label({
+    value:'Description',
+    style: styleHeader,
+  }),
+  ui.Label({
+    value: par1,
+    style: styleText
+  }),
+  par2a, par2b,
+  par3a, par3b, chambersLink,
+  ui.Label({
+    value:'How to Use',
+    style: styleHeader,
+  }),
+  ui.Label({
+    value: howTo,
+    style: styleText
+  }),
+  bulletsPanel,
+  ui.Label({
+    value:'Disclaimer',
+    style: styleHeader,
+  }),
+  ui.Label({
+    value: 'Although the underlying data and article have been reviewed and published,' + 
+    ' this app is not official and comes with no warranty.',
+    style: styleText
+  }),
+  ui.Label({
+    value: 'App created by Martin Holdrege',
+    style: {fontSize: '10px', margin: margin}
+  }),
+]);
+
+//Add this new panel to the larger panel we created 
+panel.add(description);
+
+// Add our main panel to the root of our GUI
+ui.root.insert(1,panel);
 
 // panel  for c9 legend
 map.add(fig.legendc9).add(fig.legendNumGcm);
