0a7a4a9bbeeccf4c8e1e9cbc426501e726c7a22b
martinholdrege <martinholdrege@gmail.com>
2024-10-24 20:19:34 +0000

    Updated src/general_functions.js (added 21 lines, deleted 1 line).

--- src/general_functions.js
+++ src/general_functions.js
@@ -20,4 +20,24 @@ exports.updateDict = function(originalDict, key, newValue) {
   newDict[key] = newValue;
 
   return newDict;
-}
+};
+
+// takes a dictionary, and list of keys in that dictionary,
+// returns copy of dictionary with specified keys removed. 
+var updateDict = function(originalDict, keysToRemove) {
+  var newDict = {}; // Create an empty object
+
+  // Copy all key-value pairs from the original dictionary to the new one
+  for (var k in originalDict) {
+    if (originalDict.hasOwnProperty(k)) {
+      newDict[k] = originalDict[k];
+    }
+  }
+
+  // Update the specified key with the new value
+  keysToRemove.forEach(function(key) {
+    delete newDict[key];
+  });
+
+  return newDict;
+};
