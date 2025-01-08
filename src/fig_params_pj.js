bc46e4a818bf0edf99c61ed01310ed32a38eecbd
martinholdrege <martinholdrege@gmail.com>
2025-01-08 19:24:18 +0000

    Updated src/fig_params_pj.js (added 42 lines, deleted 9 lines).

--- src/fig_params_pj.js
+++ src/fig_params_pj.js
@@ -1,9 +1,42 @@
-// Load some raster data: CONUS mean daily max temperature for January 2010
-var tmax = ee.Image('OREGONSTATE/PRISM/AN81m/201001').select('tmax');
+/*
+  Purpose: functions and other elements for visualizations of PJ niche app
 
+  Author: Martin Holdrege
+  
+  Started: Jan 8, 2025
+*/
+
+// dependencies --------------------------------------------------------------------------
+
+// var figF = require("users/MartinHoldrege/gee_apps:src/fig_functions.js");
 // Get a palette: a list of hex strings
 var palettes = require('users/gena/packages:palettes');
-var palette = palettes.matplotlib.viridis[7];
+
+// colors and visualization parameters----------------------------------------------------------
+
+// Current suitability
+var viridis = palettes.matplotlib.viridis[7];
+
+var visCurSuit = {min: 0, max: 1, palette: viridis};
+exports.visCurSuit = visCurSuit;
+
+// delta suitability
+var warmcool = palettes.misc.warmcool[7];
+warmcool[3] = '#FFFFFF'; // replacing the middle color with white
+var visDeltaSuit = {min: -1, max: 1, palette: warmcool};
+
+exports.visDeltaSuit = visDeltaSuit;
+
+// robust change
+
+
+var colsDeltaRobust = ['#332288', '#6699CC', '#88CCEE', '#661100', '#CC6677', 
+'#117733', '#44AA99', '#DDCC77', '#888888'];
+
+
+var labelsDeltaRobust = ['Increase always above threshold', 'Nonrobust above threshold', 'Decrease above threshold', 
+'Decrease to below threshold', 'Decrease across threshold', 'Increase to above threshold', 
+'Increase across threshold', 'Nonrobust across threshold', 'Below threshold'];
+
+
  
-// Display max temp with defined palette stretched between selected min and max
-Map.addLayer(tmax, {min: -11, max: 25, palette: palette}, 'tmax');
