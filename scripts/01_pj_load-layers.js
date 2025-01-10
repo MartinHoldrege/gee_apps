a8baf5c5a06b32642d410e9768ced59939743aee
martinholdrege <martinholdrege@gmail.com>
2025-01-10 22:50:54 +0000

    Updated scripts/01_pj_load-layers.js (added 44 lines, deleted 26 lines).

--- scripts/01_pj_load-layers.js
+++ scripts/01_pj_load-layers.js
@@ -16,36 +16,50 @@ var image = ee.Image(path + 'pinus_edulis_gee_test_layers_01072025'); // this is
 
 // names of the different types of runs
 var spD = {
-  'Pinus edulis': 'pinus_edulis'
+  'Juniperus californica' : 'juniperus_californica',
+  'Juniperus deppeana' : 'juniperus_deppeana',
+  'Juniperus monosperma' : 'juniperus_monosperma',
+  'Juniperus occidentalis' : 'juniperus_occidentalis',
+  'Juniperus osteosperma' : 'juniperus_osteosperma',
+  'Juniperus scopulorum' : 'juniperus_scopulorum',
+  'Pinus cembroides' : 'pinus_cembroides',
+  'Pinus edulis' : 'pinus_edulis',
+  'Pinus monophylla' : 'pinus_monophylla'
 };
 
 // scenarios
 var scenD = {
-  'SSP2-4.5 (2081–2100)': '245end'
+  'Current climate': 'current',
+  'SSP2-4.5 (2040–2060)': '245mid',
+  'SSP3-7.0 (2040–2060)': '370mid',
+  'SSP5-8.5 (2040–2060)': '585mid',
+  'SSP2-4.5 (2080–2100)': '245end',
+  'SSP3-7.0 (2080–2100)': '370end',
+  'SSP5-8.5 (2080–2100)': '585end',
 };
 
 var varTypeD = {
-  'Current suitability': 'current_suit',
-  'Change in suitability': 'change_suit',
-  'Change in suitability category': 'change_robust'
+  'Suitability': 'suitability',
+  'Change in suitability': 'suitability_change',
+  'Change in suitability category': 'robust_category'
 };
 
 // load functions ------------------------------------------------------------
 
 // load the image that has multipbe bands (i.e. one for each variable, and the transparency/mask layer)
-var loadImage = function(spName, scenName) {
-  var imagePath = path + 'pinus_edulis_gee_test_layers_01072025';
+var loadImage = function(spName) {
+  var imagePath = path + spD[spName] + '_suitability_layers_01102025';
   return ee.Image(imagePath);
 };
 
 var loadTransp = function(spName) {
-  var img = image;
-  var mask = img.select('current_suit').gte(ee.Image(-1))
+  var img = loadImage(spName);
+  var mask = img.select('suitability_current').gte(ee.Image(-1))
     .unmask(); // exclude non study area places
   var transparency = ee.Image(0)
     // these are places that should be covered by a white transparency layer
     // ie. to partially obscure areas where the sp. isn't currently found
-    .where(img.select('transparency').eq(1), ee.Image(0.5))
+    .where(img.select('transparency_mask_binary').eq(1), ee.Image(0.5))
     .selfMask()
     .updateMask(mask);
     
@@ -54,25 +68,29 @@ var loadTransp = function(spName) {
   return ui.Map.Layer(cover, {palette: 'white'}, 'add transparency to area outside current range');
 };
 
-var loadCurSuit =  function(spName, scenName) {
-  // scenName is just a dummy argument here, not used, to 
-  // make inputs same as other functions
-  var img = ee.Image(image).select('current_suit'); // temporary
-  var imageName = spD[spName] + '_current-suitability';
+var loadSuit =  function(spName, scenName) {
+  var image = loadImage(spName);
+  var lyrName = 'suitability_' + scenD[scenName];
+  var img = ee.Image(image).select(lyrName); 
+  var imageName = spD[spName] + '_' + lyrName;
   return ui.Map.Layer(img, figP.visCurSuit, imageName);
 };
 
 
 var loadDeltaSuit = function(spName, scenName) {
-  var img = image.select('change_suit'); // temporary
-  var imageName = spD[spName] + '_' + scenD[scenName] + '_delta-suitability';
+  var image = loadImage(spName);
+  var lyrName = 'suitability_change_' + scenD[scenName];
+  var img = ee.Image(image).select(lyrName); 
+  var imageName = spD[spName] + '_' + lyrName;
   return ui.Map.Layer(img, figP.visDeltaSuit, imageName);
 };
 
 
 var loadDeltaRobust = function(spName, scenName) {
-  var img = image.select('change_robust'); // temporary
-  var imageName = spD[spName] + '_' + scenD[scenName] + '_robust';
+  var image = loadImage(spName);
+  var lyrName = 'robust_category_' + scenD[scenName];
+  var img = ee.Image(image).select(lyrName); 
+  var imageName = spD[spName] + '_' + lyrName;
   return ui.Map.Layer(img, figP.visDeltaRobust, imageName);
 };
 
@@ -80,9 +98,9 @@ var loadDeltaRobust = function(spName, scenName) {
 
 // load functions for 'future' layers
 var loadFunsD = {
-  'current_suit': loadCurSuit,
-  'change_suit': loadDeltaSuit,
-  'change_robust': loadDeltaRobust
+  'suitability': loadCurSuit,
+  'suitability_change': loadDeltaSuit,
+  'robust_category': loadDeltaRobust
 };
 
 // loads the layers for the given variable, species and scenario
