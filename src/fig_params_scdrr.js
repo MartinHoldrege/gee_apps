6ad9c3ad2e94868888cd7b557a1348cbefeae36c
martinholdrege <martinholdrege@gmail.com>
2025-01-23 16:20:24 +0000

    Updated src/fig_params_scdrr.js (added 3 lines, deleted 2 lines).

--- src/fig_params_scdrr.js
+++ src/fig_params_scdrr.js
@@ -18,7 +18,8 @@ var figF = require("users/MartinHoldrege/gee_apps:src/fig_functions.js");
 
 // this is essentially the color scheme used by Chambers et al. https://doi.org/10.1111/csp2.13021
 
-var colsC3Rr1 = ['#559910',  '#A2CC7A', '#CFE6B8', '#3D0E98', '#967BCC', '#C8B8E5', '#93041A','#CC7A88', '#E6B9C0'];
+// var colsC3Rr1 = ['#559910',  '#A2CC7A', '#CFE6B8', '#3D0E98', '#967BCC', '#C8B8E5', '#93041A','#CC7A88', '#E6B9C0'];
+var colsC3Rr1 = ["#174f28", "#167984", "#169dd0", "#845e29", "#819185", "#7ebbd2", "#dd6a29", "#d8a386", "#d3d3d3"];
 
 // combination of SEI class and RR class ()
 var labelsC3Rr1 = ['CSA, H+M', 'CSA, ML', 'CSA, L', 'GOA, H+M', 'GOA, ML', 'GOA, L', 'ORA, H+M', 'ORA, ML', 'ORA, L'];
@@ -50,7 +51,7 @@ exports.legends = legends;
 
 // testing -------------------------------------------------------------------------------
 
-if(false) {
+if(true) {
   Map.add(legends);
 }
 
